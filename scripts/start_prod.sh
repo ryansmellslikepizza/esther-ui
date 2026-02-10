1fc7c1a6a8945291e3a62183a2a23223457e7730
pizzzafeet <ryannelwan@gmail.com>
2026-02-10 10:18:11 -0800

    updated start script

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+git pull
+
 npm run build
 # PORT=3001 npm run start
+cat ui.prod.config.js
 
 pm2 start ui.prod.config.js
 # pm2 start next.config.js
 pm2 list
+
